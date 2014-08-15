598330f3ec90ce8b69d3bbca14b2e29769bbdade
Jacob Ernst <jacobernst@Jacobs-MacBook-Air.local>
2014-08-15 10:31:36 -0500

    modified foreach.php so that it echos the value for items that are scalar

--- foreach.php
+++ foreach.php
@@ -3,27 +3,28 @@
  $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
  foreach ($things as $items) {
- 	if (is_int($items)) {
- 		echo "integer\n";
- 	}
-
- 	elseif (is_float($items)) {
- 		echo "float\n";
- 	}
-
- 	elseif (is_bool($items)) {
- 		echo "boolean\n";
- 	}
-
- 	elseif (is_array($items)) {
- 		echo "array\n";
- 	}
-
- 	elseif (is_string($items)) {
- 		echo "string\n";
- 	}
-
- 	else {
- 		echo "null\n";
+ 	if (is_scalar($items)) {
+ 		echo "$items\n";
  	}
  }
+ // 	elseif (is_float($items)) {
+ // 		echo "float\n";
+ // 	}
+
+ // 	elseif (is_bool($items)) {
+ // 		echo "boolean\n";
+ // 	}
+
+ // 	elseif (is_array($items)) {
+ // 		echo "array\n";
+ // 	}
+
+ // 	elseif (is_string($items)) {
+ // 		echo "string\n";
+ // 	}
+
+ // 	else {
+ // 		echo "null\n";
+ // 	}
+ // }
+?>
