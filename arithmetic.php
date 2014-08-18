0e27ae1fb8aede24e193d604ca85314eeb0575f6
Jacob Ernst <jacobernst@Jacobs-MacBook-Air.local>
2014-08-18 15:06:48 -0500

    Created arithmetic.php and created functions for adding, subtracting, multiplying, dividing, and modulus

---
+++ arithmetic.php
@@ -0,0 +1,33 @@
+<?php
+
+ function add($a, $b) {
+ 	echo $a + $b . PHP_EOL;
+ }
+
+add(1, 2);
+
+function subtract($a, $b) {
+ 	echo $a - $b . PHP_EOL;
+ }
+
+subtract(1, 2);
+
+function multiply($a, $b) {
+	echo $a * $b . PHP_EOL;
+ }
+
+multiply(2, 21);
+
+function divide($a, $b) {
+ 	echo $a / $b . PHP_EOL;
+ }
+
+divide(60, 2);
+
+function modulus($a, $b) {
+ 	echo $a % $b . PHP_EOL;
+ }
+
+modulus(60, 2);
+
+
